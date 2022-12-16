4a2cf9d71a344bfe5f3e060930c79380208acf63
Simone <simon.red.au@gmail.com>
2022-12-16 12:18:22 +0100

    added conditional rendering of questions

--- src/pages/candidates/index.jsx
+++ src/pages/candidates/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router'
-import { Divider, List, ListItemButton, ListItemText, ListSubheader, Paper } from '@mui/material'
+import { Divider, List, ListItemButton, ListItemText, ListSubheader, Paper, Tooltip } from '@mui/material'
 
 const commonProps = {
     sx: {
@@ -33,9 +33,11 @@ const CandidatesList = ({ onSelectCandidate, candidate }) => {
         >
             {candidates ? candidates.map(({ applicationId, name, id }) => {
                 return (
-                    <ListItemButton key={id} /* onClick={() => onSelectCandidate(applicationId)} selected={candidate === applicationId} */>
-                        <ListItemText primary={name} />
-                    </ListItemButton>
+                    <Tooltip key={id} title={applicationId ? name : "Candidate doesn't have uploaded any video yet"}>
+                        <ListItemButton key={id} /* disabled={!applicationId} */ onClick={() => onSelectCandidate(applicationId)} selected={candidate === applicationId}>
+                            <ListItemText primary={name} />
+                        </ListItemButton>
+                    </Tooltip>
                 )
             }) : null}
 
@@ -81,17 +83,20 @@ const CandidatePreview = ({ candidate: candidateId, question: questionId }) => {
 
 
 export const Candidates = () => {
-    const [candidate, setCandidate] = useState()
-    const [question, setQuestion] = useState()
+    const [candidate, setCandidate] = useState(null)
+    const [question, setQuestion] = useState(null)
+    const applications = useSelector(state => state.applications)
+    const questions = useSelector(state => state.questions)
+
 
 
     return (<main>
-        <header></header>
+        <header>EMBED RECRUITMENT CANDIDATES</header>
         <section style={{ display: 'flex' }}>
             <nav style={{ width: 200, height: '100vh' }}>
                 <CandidatesList onSelectCandidate={setCandidate} candidate={candidate} />
                 <Divider />
-                <QuestionsList onSelectQuestion={setQuestion} question={question} />
+                {!!candidate ? <QuestionsList onSelectQuestion={setQuestion} question={question} /> : null}
             </nav>
             <div style={{ width: 'calc(100% - 200px)', padding: 20 }}>
                 <CandidatePreview question={question} candidate={candidate} />
